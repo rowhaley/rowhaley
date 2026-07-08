3225a63e63f9b093f391f8a160354089fce03c25
Robert Whaley <whaley@alumni.virginia.edu>
2026-07-08 19:01:49 -0400

    add audio to alaska and add CalicoBluff

--- AlaskaData.js
+++ AlaskaData.js
@@ -4,6 +4,7 @@ const alaskaData = [
 	image: 'images/YukonPutIn.jpg',
 	iW: 993,
 	iH: 780,
+	audio: 'audio/YukonPutIn.mp3',
 	arrow: {
 	    src:"images/tango-right-arrow-red.svg",
 	    alt:"Right arrow",
@@ -17,10 +18,29 @@ const alaskaData = [
 	}
     },
     {
+	text: 'Calico Bluff, Eagle Alaska',
+	image: 'images/CalicoBluffStockman1911.jpg',
+	iW: 6563,
+	iH: 3871,
+	audio: 'audio/CalicoBluff.mp3',
+	arrow: {
+	    src:"images/tango-right-arrow-red.svg",
+	    alt:"Right arrow",
+	    width:"16",
+	},
+	overlay: {
+            x: 0.689,
+            y: 0.121,
+            placement: 'RIGHT',
+            checkResize: false,
+	},
+    },
+    {
 	text: 'St Marys Take Out (Native Alaskan, Tom Cole, Don Bowman, Bob Whaley)',
 	image: 'images/YukonStMarysTakeOut.jpg',
 	iW: 5613,
 	iH: 4080,
+	audio: 'audio/StMarys.mp3',
 	arrow: {
 	    src:"images/tango-right-arrow-red.svg",
 	    alt:"Right arrow",
